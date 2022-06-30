1210b4290e216291cd955f7a187d2abaca7025f9
Abhi Dadhaniya <abhidadhaniya23@gmail.com>
2022-06-30 18:28:26 +0530

    package json updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
-  theme: {
-    fontFamily: {
-      'lato': ['Lato', 'sans-serif'],
-      'quicksand': ['Quicksand', 'sans-serif'],
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        fontFamily: {
+            lato: ["Lato", "sans-serif"],
+            quicksand: ["Quicksand", "sans-serif"],
+        },
+        container: {
+            center: true,
+        },
+        extend: {},
     },
-    container: {
-      center: true,
-    },
-    extend: {},
-  },
-  plugins: [
-    require('preline/plugin')
-  ]
-}
+};
